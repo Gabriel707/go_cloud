63ae3250ff9a2f2bbe81195ccb09d8c69d4b412f
Gabriel707 <gabrielsant95@gmail.com>
2025-03-26 17:47:30 -0300

    Golang starting point for CLI tools

--- main.go
+++ main.go
@@ -2,31 +2,8 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
 )
 
 func main() {
-	var intNum int = 32767
-	intNum = intNum + 1
-	fmt.Println(intNum)
-
-	var floatNum float64 = 12345678.9
-	fmt.Println(floatNum)
-
-	var floatNum32 float32 = 10.1
-	var intNum32 = 2
-	var result float32 = floatNum32 + float32(intNum32)
-	fmt.Println(result)
-
-	var intNum1 int = 3
-	var intNum2 int = 2
-	fmt.Println(intNum1 / intNum2)
-	fmt.Println(intNum1 % intNum2)
-
-	var myString string = "Hello " + " " + "World"
-	fmt.Println(myString)
-
-	fmt.Println(utf8.RuneCountInString("y"))
-	var myRune rune = 'A'
-	fmt.Println(myRune)
+	fmt.Println("Welcome to golang CLI tools")
 }
